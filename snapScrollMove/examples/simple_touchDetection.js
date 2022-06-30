92c40dd009428b377a31f3efb0c5899d7cc1b73b
Tiohioh <96562254+tiohioh@users.noreply.github.com>
2022-06-30 11:44:32 +0900

    Update simple_touchDetection.js

--- snapScrollMove/examples/simple_touchDetection.js
+++ snapScrollMove/examples/simple_touchDetection.js
@@ -2,7 +2,7 @@
 let DOM_header_img_in = document.getElementById("huga");//snapscrollの親要素のDOMの取得
 let touchnum = 0;//現在反応している指の数
 let ssm = new snapScrollMove(DOM_header_img_in);
-let auto_move_header_img_in = () => setInterval(() => ssm.moveTo((ssm.getIndex() + 1)) ,5000);//Intervalの設定
+let auto_move_header_img_in = () => setInterval(() => ssm.moveTo(ssm.getIndex() + 1) ,5000);//Intervalの設定
 let touchinterval = auto_move_header_img_in();//Intervalの実行
 DOM_header_img_in.addEventListener('touchstart',(e) => {//タッチ検知時
 	if(!touchnum){//(複数の指が検知したとき)最初に反応した指であるなら(touchnum != 0)
